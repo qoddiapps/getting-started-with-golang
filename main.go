67fd2be9460ead8c8e4c2cee2af0ae7019eb5d36
FlashDrive.io <73052948+flashdriveio@users.noreply.github.com>
2021-03-27 23:52:32 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+import (
+ "fmt"
+ "net/http"
+ "os"
+  _ "github.com/lib/pq"
+)
+func main() {
+ var PORT string
+ if PORT = os.Getenv("PORT"); PORT == "" {
+  PORT = "3001"
+ }
+ http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello World from path: %s\n", r.URL.Path)
+ })
+ http.ListenAndServe(":" + PORT, nil)
+}
